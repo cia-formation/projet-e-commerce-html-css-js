476d2261b97b6eccdcc799b6e1a04d92dce1d7ea
EssiJunior <nessipjunior@gmail.com>
2025-03-25 10:44:41 +0100

    Done Image carroussel

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ function openModal() {
 function closeModal() {
     modal.style.display = 'none';
 }
+
+
+// Login function
+function login() {
+    // Get the email and password values from the form
+    const email = document.querySelector('.signin-form input[type="email"]').value;
+    const password = document.querySelector('.signin-form input[type="password"]').value;
+
+    if (email === 'admin@email' && password === 'admin') {
+        window.location.href='../html/admin/liste-produits.html'
+    } else {
+        window.location.href='../html/client/liste-produits.html'
+    }
+}
